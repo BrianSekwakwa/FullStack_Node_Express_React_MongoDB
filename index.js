e5196c993c9bee77b4f72b66bb4364a5ab3c3a9e
briansekwakwa <briansenpai14@gmail.com>
2020-11-16 07:35:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send({ Hi: "There"});
+})
+
+
+// The process.env tell heroku which environment it should declare the port on
+// The boolean statement is used so that we in PRODUCTION we can use process.env but in DEVELOPMENT we can use 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
