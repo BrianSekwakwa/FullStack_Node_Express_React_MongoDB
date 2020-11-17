17de1b67666186c62d6eda7fa7842bcf26438168
briansekwakwa <briansenpai14@gmail.com>
2020-11-17 08:27:39 +0200

    installed dependecies and testing out google authentication

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const keys = require("./config/keys");
 const app = express();
 
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: "/auth/google/callback",
-  }),
-  (accessToken) => {
-    console.log(accessToken);
-  }
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("Access Token: ", accessToken);
+      console.log("Refresh Token: ", refreshToken);
+      console.log("Profile: ", profile);
+    }
+  )
 );
+
+app.get(
+  "/auth/google/",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 // The process.env tell heroku which environment it should declare the port on
 // The boolean statement is used so that we in PRODUCTION we can use process.env but in DEVELOPMENT we can use 3000
